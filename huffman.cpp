240fcbb3f3d7885e2ffce96110c2e49ff3e8266a
yanazdravkova <yana.zdravkovaa@gmail.com>
2018-01-25 00:18:56 +0200

    Add second unfinished version
    Replace usage of Tree class with simply Node pointer
    Implemented half of the functionalities

--- huffman.cpp
+++ huffman.cpp
@@ -1,15 +1,44 @@
 #include<huffman.h>
 #include <algorithm>
+#include <iomanip>
+#include<string>
 using namespace std;
-bool sortBySecond(const pair<int,int> &a,const pair<int,int> &b)
+bool sortBySecond(const Node* a,const Node* b)
 {
-    return (a.second > b.second);
+    return (a->data.second < b->data.second);
 }
+//направи го шаблонно
 void printMap(unordered_map<char, int> m)
 {
-    for ( auto it = m.begin(); it != m.end(); ++it )
-        cout << " " << it->first << ":" << it->second;
+     for ( const pair<char,int> p : m )
+    {
+        cout << " " << p.first << ":"<< p.second;
+    }
     cout <<endl;
+}
+void printMap(unordered_map<char, string> m)
+{
+    for ( const pair<char,string> p : m )
+    {
+        cout<<" " << p.first << ":"<< p.second;
+    }
+    cout <<endl;
+}
+int binToDec(string bin)
+{
+    int result = 0;
+    for(int i = 0; i<bin.size(); i++)
+    {
+        if(bin.at(i) == '1')
+        {
+            if(i == (bin.size() - 1))
+                result += 1;
+            else
+                result += (pow(2,bin.size()-1-i));
+
+        }
+    }
+    return result;
 }
 void Huff::setInputFile(string name)
 {
@@ -31,41 +60,161 @@ void Huff::createFrequencyTable()
         //за да не чете последния символ 2 пъти
         if(fin.eof())
             break;
-        if(table.find(c) == table.end())
-            table[c] = 1;
+        if(frequencyTable.find(c) == frequencyTable.end())
+            frequencyTable[c] = 1;
         else
-            table[c]++;
+            frequencyTable[c]++;
+    }
+    fin.close();
+}
+bool Huff::isMemberHelp(Node* const& r, char c) const
+{
+    if(r == nullptr)
+        return false;
+    if(c == r->data.first)
+        return true;
+    return isMemberHelp(r->left, c) || isMemberHelp(r->right, c);
+}
+bool Huff::isMember(char c) const
+{
+    return isMemberHelp(root, c);
+}
+string Huff::binaryCodeHelp(const char c, const Node* start) const
+{
+    if(c == start->data.first)
+        return "";
+    if(start == nullptr)
+        return "";
+    if(isMemberHelp(start->left,c))
+        return "0" + binaryCodeHelp(c, start->left);
+    if(isMemberHelp(start->right,c))
+        return "1" + binaryCodeHelp(c, start->right);
+    return "";//ако символът не се съдържа изобщо в дървото с корен start
+}
+string Huff::binaryCode(const char c) const
+{
+    return binaryCodeHelp(c,root);
+}
+
+void Huff::createCodeTable()
+{
+    if(root == nullptr)
+        return;
+
+    for( pair<char,int> p : frequencyTable)
+    {
+        codeTable.emplace(p.first,binaryCode(p.first));
     }
 }
+void Huff::saveBinaryCode() const
+{
+    ifstream fin;
+    ofstream fout;
+    char c;
+    fin.open(inputFile, ios::in);
+    fout.open(outputFile, ios::out | ios::app);
+    while(true)
+    {
+        fin.get(c);
+        //за да не чете последния символ 2 пъти
+        if(fin.eof())
+            break;
+        for(const pair<char,string> p : codeTable)
+        {
+            if(p.first == c)
+            {
+                fout<<p.second;
+            }
+        }
+    }
+    fin.close();
+    fout.close();
+}
+//връща като резултат възела с най-малка числена стойност като същевременно го премахва от списъка с възели, подаден като параметър
+Node*& Huff::findMinNode(list<Node*>& nodes)
+{
+    nodes.sort(sortBySecond);
+    return nodes.front();
+}
+
 void Huff::buildTree()
 {
-    vector<pair<char,int>> v;
-    for(const pair<char,int> p : table)
-        v.push_back(p);
-    //сортираме вектора по втори елемент на двойката
-    sort(v.begin(), v.end(), sortBySecond);
-    /* for (const auto& p : v)
-     {
-         cout << p.first << ", " << p.second << endl;
-     }*/
+    //стъпка 2 от построяване на дърво на Хъфман от описанието
+    //създаваме n дървета от по един възел, който съдържа наредена двойка и ги записваме в списък, като той е сортиран във низходяш
+    list<Node*> nodes;
+    for(const pair<char,int> p : frequencyTable)
+    {
+        nodes.push_front(new Node(p));
+    }
+   /* nodes.sort(sortBySecond);
+    cout<<nodes.front()->data.first<<" "<<nodes.front()->data.second<<endl;*/
+    while(nodes.size() > 1)
+    {
+        Node* fst = findMinNode(nodes);
+        nodes.pop_front();//премахваме fst от списъка
+        Node* snd = findMinNode(nodes);
+        nodes.pop_front();//премахваме snd от списъка
+        Node* res = new Node(make_pair(' ',fst->data.second + snd->data.second), fst, snd);//строим ново дърво с поддървета 2те най-малки досега
+        nodes.push_front(res);
+    }
+    //има само 1 възел в списъка и това е нашият корен
+  //  root = nodes.pop_front();
 
+  root = nodes.front();
 }
-void Huff::printTree(ostream& out = cout, int currentHeight = 0) const
+void Huff::saveDecCode()const
 {
-    tree.prettyPrint(out,currentHeight);
+    ofstream fout;
+    fout.open(outputFile, ios::out|ios::app);
+    list<string> subs;
+    for(int i=0; i<code.size(); i + 8)
+    {
+        subs.push_back( code.substr(i,8));
+        cout<<code.substr(i,8);
+    }
+    for(const string s : subs)
+    {
+        int tmp = binToDec(s);
+        cout<<tmp<<endl;
+        fout<<tmp;
+    }
+    fout.close();
 
 }
-void Huff::printTable() const
+void prettyPrint(ostream& out = cout,int currentHeight = 0, Node* start = nullptr)
 {
-    printMap(table);
-}
+    if(start ==nullptr)
+        return;
+    prettyPrint(out, currentHeight + 1, start->right);
+    if(start->data.first == ' ')
+        out<<setw(5*currentHeight)<<" "<<start->data.second<<endl;
+    else
+        out<<setw(5*currentHeight)<<" "<<start->data.first<<" "<<start->data.second<<endl;
+    prettyPrint(out, currentHeight + 1,start->left);
 
+}
+void Huff::printTree() const
+{
+    prettyPrint(cout,0,root);
+
+}
+void Huff::printFrequencyTable() const
+{
+    printMap(frequencyTable);
+}
+void Huff::printCodeTable() const
+{
+    printMap(codeTable);
+}
 int main()
 {
     Huff h;
     h.setInputFile("input.txt");
+    h.setOutputFile("output.txt");
     h.createFrequencyTable();
-    h.printTable();
-    cout<<"kotka";
+    h.buildTree();
+    h.createCodeTable();
+    h.saveBinaryCode();
+    h.saveDecCode();
     return 0;
 }
--- huffman.h
+++ huffman.h
@@ -1,34 +1,51 @@
+
 #ifndef _HUFFMAN_
 #define _HUFFMAN_
 
 #include <iostream>
-#include<huffmanTree.cpp>
+
 #include<string>
 #include<fstream>
 #include<unordered_map>
 #include<set>
-#include<vector>
+#include<list>
 using namespace std;
+struct Node
+{
+    pair<char,int> data;
+    Node* left;
+    Node* right;
+    Node():data(), left(nullptr), right(nullptr) {}
+    Node(const pair<char,int>& data, Node* left = nullptr, Node* right = nullptr)
+        : data(data), left(left), right(right) {}
+};
 class Huff
 {
-    BinaryTree tree;
-    unordered_map<char,int> table;
-
+    Node* root;
+    unordered_map<char,int> frequencyTable;
+    unordered_map<char,string> codeTable;
     string inputFile;
     string outputFile;
-
-
+    string code;
+    bool isMemberHelp(Node*const& r, char c) const;
+    string binaryCodeHelp(const char c, const Node* start) const;
 public:
-    Huff() {}
-    Huff(BinaryTree _tree, unordered_map<char,int> _table, string _inputFile, string _outputFile):tree(_tree),
-        table(_table),inputFile(_inputFile),outputFile(_outputFile) {}
+    Huff():root(nullptr), frequencyTable(), codeTable(), inputFile(""), outputFile(""), code("") {}
+    Huff(Node* _root, unordered_map<char,int> _frequencyTable, unordered_map<char,string> _codeTable, string _inputFile, string _outputFile, string _code):root(_root),
+        frequencyTable(_frequencyTable),codeTable(_codeTable),inputFile(_inputFile),outputFile(_outputFile), code(_code) {}
     void setInputFile(string name);
     void setOutputFile(string name);
     void createFrequencyTable();
+    void createCodeTable();
     void buildTree();
-    void printTree(ostream& out, int currentHeight) const;
-    void printTable() const;
+    void printTree() const;
+    void printFrequencyTable() const;
+    void printCodeTable() const;
+    bool isMember( char c) const;
+    Node*& findMinNode(list<Node*>& nodes);
+    string binaryCode(const char c) const;
+    void saveBinaryCode() const;
+    void saveDecCode() const;
+
 };
 #endif // _HUFFMAN_
-
-
